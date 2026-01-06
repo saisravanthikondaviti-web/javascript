b26c034db5f6d4afdbea4434903a5c49d78aa065
saisravanthikondaviti-web <saisravanthikondaviti@gmail.com>
2026-01-06 12:08:42 +0530

    Add files via upload

---
+++ gradecalculator.js
@@ -0,0 +1,16 @@
+let marks=59
+if(marks>=90)
+{
+    console.log("your grade is A");
+}
+else if(marks>=75)
+{
+    console.log("your grade is B");
+}
+else if(marks>=60)
+{
+    console.log("your grade is C");
+} 
+else {
+    console.log("you are failed!!");
+}
