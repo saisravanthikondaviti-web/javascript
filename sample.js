c2c2b6dda11b8b31d7aaacb20f549b7333a66f18
saisravanthikondaviti-web <saisravanthikondaviti@gmail.com>
2026-01-06 11:41:47 +0530

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,7 @@
+let name="sravanthi";
+const age=20;
+console.log("Age:",age,"datatype:",typeof(age));
+console.log("Name:",name,"datatype:",typeof(name));
+let isstudent=true;
+console.log("Isstudent:",isstudent,"datatype:",typeof(isstudent));
+
