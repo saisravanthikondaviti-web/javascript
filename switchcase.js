50674eaa72804f51adcf8b39323d4df1afe4408d
saisravanthikondaviti-web <saisravanthikondaviti@gmail.com>
2026-01-06 12:23:51 +0530

    Add files via upload

---
+++ switchcase.js
@@ -0,0 +1,33 @@
+let day=3
+switch(day)
+{
+    case 1:
+        if(day==1)
+            console.log("Monday");
+    break;
+    case 2:
+        if(day==2)
+            console.log("Tuesday");
+    break;
+    case 3:
+        if(day==3)
+        console.log("Wednesday");
+    break;
+    case 4:
+        if(day==4)
+            console.log("Thursday");
+    break;
+    case 5:
+        if(day==5)
+            console.log("Friday");
+    break;
+    case 6:
+        if(day==6)
+            console.log("Saturday");
+    break;
+    case 7:
+        if(day==7)
+            console.log("Sunday");
+    default:
+        console.log("Good day!!")
+}
