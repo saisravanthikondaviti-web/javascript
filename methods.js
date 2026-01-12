17efbd89e1a1403dcbc425362146dd94f192fdc1
saisravanthikondaviti-web <saisravanthikondaviti@gmail.com>
2026-01-12 12:40:27 +0530

    Update methods.js

--- methods.js
+++ methods.js
@@ -1,3 +1,8 @@
+//--------------------------
+//String Methods
+//--------------------------
+
+//function to calculate sum of array elements
 function sum(){
     let number=[10,20,30,40,50];
     let total=0;
@@ -45,3 +50,80 @@ console.log("Includes 'world':",includesSubstring("Hello World","world"));
 //output:
 // Includes 'World': true
 // Includes 'world': false
+
+
+
+//--------------------------
+//Array Methods
+//--------------------------
+
+// function to add element at the end
+function addelement(){
+    let arr=[1,2,3];
+    arr.push(4);
+    return arr;
+}
+console.log("Array after push:",addelement());
+//output:
+// Array after push: [ 1, 2, 3, 4 ]
+
+//function to remove last element
+function removeelement(){
+    let arr=[1,2,3,4];
+    arr.pop();
+    return arr;
+}
+console.log("Array after pop:",removeelement());
+//output:
+// Array after pop: [ 1, 2, 3 ]
+
+//function to add element at the beginning
+function addAtBeginning(){
+    let arr=[5,6,7,8];
+    arr.unshift(4);
+    return arr;
+}
+console.log("Array after unshift:",addAtBeginning());
+//output:
+// Array after unshift: [ 4, 5, 6, 7, 8 ]
+
+//function to remove first element
+function removeFromBeginning(){
+    let arr=[4,5,6,7,8];
+    arr.shift();
+    return arr;
+}
+console.log("Array after shift:",removeFromBeginning());
+//output:
+// Array after shift: [ 5, 6, 7, 8 ]
+
+//map method
+function mapArray(){
+    let arr=[4,5,6,7,8];
+    let squared=arr.map(num=>num*num);
+    return squared;
+}
+console.log("Squared array:",mapArray());
+// //output:
+// Squared array: [ 16, 25, 36, 49, 64 ]
+
+//filter method
+function filterArray(){
+    let arr=[10,15,20,25,30];
+    let filtered=arr.filter(num=>num>20);
+    return filtered;
+}
+console.log("Filtered array:",filterArray());
+// //output:
+// Filtered array: [ 25, 30 ]
+
+//foreach method
+function forEachArray(){
+    let arr=[1,2,3,4,5];
+    let product=1;
+    arr.forEach(num=>product*=num);
+    return product;
+}
+console.log("Product using forEach:",forEachArray());
+// //output:
+// Product using forEach: 15
